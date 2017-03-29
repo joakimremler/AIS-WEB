05bfe08a6de6644462a38abfd5b1e2952ca830e0
Joakim <joakim@idrottskoll.se>
2017-03-29 22:03:37 +0200

    Brand new repo

---
+++ AIS-WEB/Controllers/PlantController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AIS_WEB.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AIS_WEB.Controllers
+{
+    public class PlantController : Controller
+    {
+        //private List<PlantViewModel> plants;
+        //private PlantViewModel plant;
+
+        // GET: Plant
+        public ActionResult Index()
+        {
+            //Get all plants from API
+            //client.BaseAddress = new Uri("http://localhost:61347/");
+            
+
+            return View();
+        }
+
+        //localhost:52844/Plant/PlantsJs
+        public ActionResult PlantsJS ()
+        {
+            
+            return View();
+       
+        }
+
+        public ActionResult PlantOrNot()
+        {
+            return View();
+
+        }
+
+        public ActionResult GetPlant()
+        {
+            PlantViewModel plant = new PlantViewModel();
+            System.Diagnostics.Debug.WriteLine("start");
+            System.Diagnostics.Debug.WriteLine(Json(plant.getPlantAPI(), JsonRequestBehavior.AllowGet));
+            return Json(plant.getPlantAPI(), JsonRequestBehavior.AllowGet);
+
+        } 
+    }
+}
---
+++ AIS-WEB/Models/PlantViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace AIS_WEB.Models
+{
+    public class PlantViewModel
+    {
+        public Object getPlantAPI()
+        {
+            string url = "http://localhost:61347/api/Plant";
+            var client = new System.Net.WebClient();
+            //download API as a string
+            var content = client.DownloadString(url);
+            var sterializer = new JavaScriptSerializer();
+            //read from string and change it to json
+            var jsonContent = sterializer.Deserialize<Object>(content);
+            System.Diagnostics.Debug.WriteLine("start2");
+            System.Diagnostics.Debug.WriteLine(jsonContent);
+            return jsonContent;
+        }
+        /*
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int SoilMoistureMin { get; set; }
+        public int SoilMoistureMax { get; set; }
+        public int SoilMoistureNow { get; set; }
+        public string Category { get; set; }
+        public int SunlightMin { get; set; }
+        public int SunlightMax { get; set; }
+        public int SunlightNow { get; set; }
+        public int ID_Type { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime Updated { get; set; }
+        */
+    }
+}
