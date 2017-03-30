0b56830d5cc05c6922ba0d998511cbe73f56076b
Joakim <joakim@idrottskoll.se>
2017-03-30 15:16:28 +0200

    working on posting to api

--- AIS-WEB/Controllers/PlantController.cs
+++ AIS-WEB/Controllers/PlantController.cs
@@ -8,7 +8,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
-
+using System.Text;
 
 namespace AIS_WEB.Controllers
 {
@@ -52,7 +52,86 @@ namespace AIS_WEB.Controllers
         [HttpPost]
         public  ActionResult Create(PlantViewModel plant)
         {
+
+            using (HttpClient client = new HttpClient())
+            {
+                //Connect to server
+                client.BaseAddress = new Uri("http://localhost:61347/");
+
+                //Clears old data???
+                client.DefaultRequestHeaders.Accept.Clear();
+
+                PlantViewModel newPlant = new PlantViewModel();
+                newPlant.Name = plant.Name;
+                newPlant.SoilMoistureMin = plant.SoilMoistureMin;
+                newPlant.SoilMoistureMax = plant.SoilMoistureMax;
+                newPlant.SoilMoistureNow = plant.SoilMoistureNow;
+                newPlant.Category = plant.Category;
+                newPlant.SunlightMin = plant.SunlightMin;
+                newPlant.SunlightMax = plant.SunlightMax;
+                newPlant.SunlightNow = plant.SunlightNow;
+                newPlant.ID_Type = plant.ID_Type;
+                newPlant.StartDate = plant.StartDate;
+                newPlant.Updated = plant.Updated;
+                //string url = String.Format("http://localhost:61347/api/Plant/{0}", plant.ID);
+
+                var response = client.PostAsync("http://localhost:61347/api/Plant/" + plant.ID, new StringContent(
+                new JavaScriptSerializer().Serialize(newPlant), Encoding.UTF8, "application/json")).Result;
+
+
+            }
+
             return View();
+
+
+
+
+
+            /*
+            var client = new HttpClient();
+
+            //Connect to server
+            client.BaseAddress = new Uri("http://localhost:61347/");
+
+            //Clears old data???
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            PlantViewModel newPlant = new PlantViewModel();
+            newPlant.Name = plant.Name;
+            newPlant.SoilMoistureMin = plant.SoilMoistureMin;
+            newPlant.SoilMoistureMax = plant.SoilMoistureMax;
+            newPlant.SoilMoistureNow = plant.SoilMoistureNow;
+            newPlant.Category = plant.Category;
+            newPlant.SunlightMin = plant.SunlightMin;
+            newPlant.SunlightMax = plant.SunlightMax;
+            newPlant.SunlightNow = plant.SunlightNow;
+            newPlant.ID_Type = plant.ID_Type;
+            newPlant.StartDate = plant.StartDate;
+            newPlant.Updated = plant.Updated;
+            string url = String.Format("http://localhost:61347/api/Plant/{0}", plant.ID);
+
+            //update api
+            var response = client.PostAsync("api/Plant", new StringContent(
+             new JavaScriptSerializer().Serialize(newPlant), Encoding.UTF8, "application/json")).Result;
+             */
+
+
+
+
+            //client.PostAsJsonAsync("api/Plant", newPlant).Result;
+
+            //await client.PutAsJsonAsync(url, newPlant);
+
+
+            /*
+            using (var client2 = new HttpClient())
+                {
+                    var response = client2.PostAsJsonAsync("api/Plant", newPlant).Result;
+
+                }
+            */
+
+
         }
 
         [HttpGet]
