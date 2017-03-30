c46f9a1ec7592b3d5f1ed9f98124371395b16a73
Joakim <joakim@idrottskoll.se>
2017-03-30 14:24:44 +0200

    Added edit page

--- AIS-WEB/Controllers/PlantController.cs
+++ AIS-WEB/Controllers/PlantController.cs
@@ -40,23 +40,11 @@ namespace AIS_WEB.Controllers
         {
             PlantViewModel plant = new PlantViewModel();
             var result = plant.getPlantAPI();
-
-            System.Diagnostics.Debug.WriteLine("testing");
-            System.Diagnostics.Debug.WriteLine(result);
-
             return View(result);
-            //return View();
-
         }
 
         public ActionResult GetPlant()
-        {/*
-            PlantViewModel plant = new PlantViewModel();
-            var result = plant.getPlantAPI();
-
-            System.Diagnostics.Debug.WriteLine("testing");
-            System.Diagnostics.Debug.WriteLine(result);
-            */
+        {
             return View();
 
         }
@@ -70,22 +58,9 @@ namespace AIS_WEB.Controllers
         [HttpGet]
         public ActionResult Edit(string id)
         {
-
-
-            string url = "http://localhost:61347/api/Plant/" + id;
-            System.Diagnostics.Debug.WriteLine("start");
-            System.Diagnostics.Debug.WriteLine(url);
-            /*
-            var client = new System.Net.WebClient();
-            //download API as a string
-            var content = client.DownloadString(url);
-            var sterializer = new JavaScriptSerializer();
-            //read from string and change it to json
-            var jsonContent = sterializer.Deserialize<Object>(content);
-            //System.Diagnostics.Debug.WriteLine("start2");
-            //System.Diagnostics.Debug.WriteLine(jsonContent);
-            return View(jsonContent);*/
-            return View();
+            PlantViewModel plant = new PlantViewModel();
+            var result = plant.getPlant(id);
+            return View(result);
         }
 
         [HttpPost]
--- AIS-WEB/Models/PlantViewModel.cs
+++ AIS-WEB/Models/PlantViewModel.cs
@@ -28,38 +28,24 @@ namespace AIS_WEB.Models
         
         public List<PlantViewModel> getPlantAPI()
         {
-            
-
         string url = "http://localhost:61347/api/Plant";
             var client = new System.Net.WebClient();
             //download API as a string
             var content = client.DownloadString(url);
             var sterializer = new JavaScriptSerializer();
-
             plants = sterializer.Deserialize<List<PlantViewModel>>(content).ToList();
-
-            //return routes_list;
-            System.Diagnostics.Debug.WriteLine("testing22");
-            System.Diagnostics.Debug.WriteLine(plants);
-
             return plants;
+        }
 
-        /*
-            string url = "http://localhost:61347/api/Plant/1";
+        public PlantViewModel getPlant(string id)
+        {
+            string url = "http://localhost:61347/api/Plant/" + id;
             var client = new System.Net.WebClient();
             //download API as a string
             var content = client.DownloadString(url);
             var sterializer = new JavaScriptSerializer();
-
-            PlantViewModel PlantObject = sterializer.Deserialize<PlantViewModel>(content);
-
-            System.Diagnostics.Debug.WriteLine("testing");
-            System.Diagnostics.Debug.WriteLine(PlantObject.Name);
-
-
-            //return routes_list;
-            return PlantObject;
-            */
+            plantitem = sterializer.Deserialize<PlantViewModel>(content);
+            return plantitem;
         }
 
     }
